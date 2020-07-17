8400089dd482bd41aa7cac2a6586ce640a523fa9
Zietieflr <lrmcguire93@gmail.com>
2020-07-17 17:13:53 -0600

    Add form component, tested. Add TTodos from form render. Add Delete button, tested. Light styling.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoContainer from './components/TodoContainer'
+import TodoForm from './components/TodoForm'
 const todosURL = "http://localhost:3000/todos"
 
 class App extends Component {
@@ -21,11 +22,36 @@ class App extends Component {
       }) )
   }
 
+  addTodo = (newTodo) => {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+
+    fetch(todosURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTodo)
+    })
+  }
+
+  deleteTodo = (id) => {
+    let filtered = this.state.todos.filter(todo => todo.id !== id)
+    
+    this.setState({
+      todos: filtered
+    })
+
+    fetch(todosURL + `/${id}`, {method: 'DELETE'})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Todo App</h1>
-        <TodoContainer todos={this.state.todos} />
+        <TodoForm addTodo={this.addTodo} /> 
+        <TodoContainer deleteTodo={this.deleteTodo} todos={this.state.todos} />
       </div>
     );
   }
