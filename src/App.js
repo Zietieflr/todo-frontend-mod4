d653746e23ec77625f7bae1c1cc3e961c59339ae
Zietieflr <lrmcguire93@gmail.com>
2020-07-27 16:35:10 -0600

    Add form for new user. Style form. Validation ffeedback for user.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { postTodo, patchTodo, deleteTodo } from './helpers/functions'
 import TodoContainer from './components/TodoContainer'
 import TodoForm from './components/TodoForm'
+import SignupForm from './components/SignupForm'
 const todosURL = "http://localhost:3000/todos"
 
 class App extends Component {
   
   state = {
     todos: [],
+    user: {},
+    alerts: [],
   }
 
   componentDidMount(){
@@ -49,10 +52,34 @@ class App extends Component {
     deleteTodo(id)
   }
 
+  signUp = (user) => {
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({user})
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response.errors) {
+          this.setState({alerts: response.errors})
+        }
+        else {
+          localStorage.setItem('token', response.token)
+          this.setState({
+            user: response.user,
+            alerts: ['User successfully created!']
+          })
+        }
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Todo App</h1>
+        <SignupForm signUp={this.signUp} alerts={this.state.alerts} />
         <TodoForm submitAction={this.addTodo} /> 
         <TodoContainer updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} todos={this.state.todos} />
       </div>
